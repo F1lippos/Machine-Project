36002cacf98341523b1edabe222a625fb94865ad
Filippos Pikrides <filipos240498@gmail.com>
2021-12-11 16:05:28 +0000

    Load all csv files

--- src/projectCSC8635.py
+++ src/projectCSC8635.py
@@ -41,3 +41,44 @@ dat['age'].value_counts().plot(kind='bar', title='Age',figsize=(20,8))
 dat['weight'].value_counts().plot(kind='bar', title='Weight',figsize=(20,8)) 
 
 sns.regplot(x=dat['weight'], y=dat['height'])
+
+# read many files csv 
+import os
+print(os.listdir("C:/Users/Filippos/Downloads/A_DeviceMotion_data/A_DeviceMotion_data"))
+
+folders = glob('C:/Users/Filippos/Downloads/A_DeviceMotion_data/A_DeviceMotion_data/*_*')
+folders = [s for s in folders if "csv" not in s]
+df_all_list = []
+activity_codes = {'dws':0,'jog':1,'sit':2,'std':3,'ups':4,'wlk':5}
+activity_types = list(activity_codes.keys())
+
+for j in folders:
+    print('j',j)
+    csv = glob(j + '/*')
+    for i in csv:
+        df = pd.read_csv(i)
+        df['activity'] = activity_codes[j[68:71]]
+        df['sub_num'] = i[len(j)+5:-4]
+       
+        df_all_list.append(df)
+        
+        
+df_all = pd.concat(df_all_list,axis=0)
+df_all = df_all.drop('Unnamed: 0',axis=1)
+print(df_all.shape)
+print(df_all.columns)
+
+
+
+fig, axs = plt.subplots(6,sharex=True, sharey=True)
+
+fig.suptitle('Vertically stacked subplots')
+activitycolors={0:'red',1:'orange',2:'green',3:'blue',4:'red',5:'grey'}
+#Visualization
+for act in activity_types: 
+    axs[activity_codes[act]].set_title(activity_types[activity_codes[act]])
+    df = df_all[(df_all['sub_num']=='3') & (df_all['activity']==activity_codes[act])]
+    str1= 'tab:' 
+    str2 =  activitycolors[activity_codes[act]] 
+    vcolor =  str1+str2 
+    axs[activity_codes[act]].plot(df_all['userAcceleration.x'][:500],vcolor)
